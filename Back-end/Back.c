3cd4093d463dd654964818d960d5e891c55f61ad
Michele Acciuffi <micheleacciuffi@gmail.com>
2025-04-24 10:50:38 +0200

    Implementazione delle prime due funzioni per il dll

---
+++ Back-end/Back.c
@@ -0,0 +1,24 @@
+#include "Michele.h"
+
+__declspec(dllexport) int Pareggio(char Tavola[3][3]) {
+    for (int Riga = 0; Riga < 3; Riga++) {
+        for (int Colonna = 0; Colonna < 3; Colonna++) {
+            if (Tavola[Riga][Colonna] == ' ') {
+                return -1;
+            }
+        }
+    }
+    return 1;
+}
+
+__declspec(dllexport) int Vittoria(char Tavola[3][3]) {
+    for (int Riga = 0; Riga < 3; Riga++) {
+        if (Tavola[Riga][0] == Tavola[Riga][1] && Tavola[Riga][1] == Tavola[Riga][2] && Tavola[Riga][0] != ' ') return 1;
+    }
+    for (int Colonna = 0; Colonna < 3; Colonna++) {
+        if (Tavola[0][Colonna] == Tavola[1][Colonna] && Tavola[1][Colonna] == Tavola[2][Colonna] && Tavola[0][Colonna] != ' ') return 1;
+    }
+    if (Tavola[0][0] == Tavola[1][1] && Tavola[1][1] == Tavola[2][2] && Tavola[0][0] != ' ') return 1;
+    if (Tavola[0][2] == Tavola[1][1] && Tavola[1][1] == Tavola[2][0] && Tavola[0][2] != ' ') return 1;
+    return -1;
+}
--- main.lua
+++ main.lua
@@ -1,5 +1,25 @@
 local ffi = require("ffi")
 ffi.cdef[[
-    
+    int Pareggio(char Tavola[3][3]);
+    int Vittoria(char Tavola[3][3]);
 ]]
 local Backend = ffi.load("Back-end\\Back.dll")
+
+local Tabella = {
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}
+}
+local Menu
+
+function Tabella_Lua_C(Tavola)
+    local CArray = ffi.new("char[3][3]")
+    for i = 1, 3 do
+        for j = 1, 3 do
+            CArray[i-1][j-1] = Tavola[i][j]:byte()
+        end
+    end
+    return CArray
+end
+
+print(Backend.Vittoria(Tabella_Lua_C(Tabella)))
